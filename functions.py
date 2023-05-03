548b46fd16c0e1a2ceefcfb378e672c125df7254
Giacomo Rossi <115655415+giakomorssi@users.noreply.github.com>
2023-05-03 18:07:51 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -24,7 +24,7 @@ def prediction_pkl_st(model, df, threshold = 0.1):
 
   with st.spinner('Wait for it...'):
     st.subheader('\nModel:\n')
-    st.write(model)
+    st.write("VotingClassifier(estimators=[('xgb', XGBClassifier()), ('lr', LogisticRegression(class_weight='balanced', max_iter=1000))], voting='soft')")
    
   st.write(f'The Within Threshold is: {threshold} that corresponds to {np.round(24*threshold, 2)} hours.')
 
